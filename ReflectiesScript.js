f70ff022a684b4aeb0e6bb771782f32ffc95485a
QuentinBakkerPXL <114091462+QuentinBakkerPXL@users.noreply.github.com>
2023-06-10 21:27:15 +0200

    more stuffers

--- ReflectiesScript.js
+++ ReflectiesScript.js
@@ -10,3 +10,33 @@ toggleBtn.onclick = function () {
         ? 'fa-solid fa-xmark'
         : 'fa-solid fa-bars'
 }
+function myFunction() {
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more");
+    let btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+function myFunction2() {
+    let dots = document.getElementById("dots2");
+    let moreText = document.getElementById("more2");
+    let btnText = document.getElementById("myBtn2");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
